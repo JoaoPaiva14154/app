d9805e4051ff16607f3fce42aa22f778a06650b5
João Paiva <JoaoPaiva@evensimpler.pt>
2021-06-06 11:54:47 +0100

    Lesson-6

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(
+ "mongodb+srv://eva:<password>@cluster0-2zrmd.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("connected do db");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 
 app.use((req, res, next) => {
@@ -15,21 +44,43 @@ app.use((req, res, next) => {
 
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added",
   });
 });
 
-app.get('/api/posts', (req, res, next) => {
-    const posts = [
-         { id: '23hr23r8', title: 'First server-side post', author: 'catzilla', content: 'This is coming from the server'},
-         { id: 'fd7yfdyf', title: 'Second server-side post', author: 'dogge', content: 'This is also coming from the server'}
-    ];
-    res.status(200).json({
-        message: 'Post sent with success!',
-        posts: posts
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: "Post sent with success!",
+        posts: documents,
+      });
+    })
+    .catch(() => {
+      res.status(201).json({
+        message: "There was an error fetching posts",
+      });
+    });
+});
+
+app.delete("/api/posts", (req, res, next) => {
+  Post.delete()
+    .then((documents) => {
+      res.status(200).json({
+        message: "Post deleted successfuly!"
+      });
+    })
+    .catch(() => {
+      res.status(201).json({
+        message: "There was an error deleting the post",
+      });
     });
 });
 
---
+++ models/post.js
@@ -0,0 +1,8 @@
+const mongooose = require("mongoose");
+ 
+const postSchema = mongoose.Schema({
+ title: { type: String, required: true },
+ content: { type: String, required: true },
+});
+ 
+module.exports = mongoose.model("Post", postSchema);
